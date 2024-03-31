3c96e8e41efc13442880383b3e2fdd5fa531ed96
thegreatcodeholio <ian@icarey.net>
2024-03-30 22:53:15 -0400

    redis password fix added '' around randomly generated password.

--- icad_manager.py
+++ icad_manager.py
@@ -87,7 +87,7 @@ TIMEZONE="America/New_York"
 # Redis Configuration
 REDIS_HOST={config_data["redis"]["host"]}
 REDIS_PORT={config_data["redis"]["port"]}
-REDIS_PASSWORD={config_data["redis"]["password"]}
+REDIS_PASSWORD='{config_data["redis"]["password"]}'
 
 # MySQL Configuration
 MYSQL_ROOT_PASSWORD='{generate_password()}'
